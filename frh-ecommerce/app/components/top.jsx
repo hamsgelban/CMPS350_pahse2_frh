e49e3e04b0ef1356e401099e8da740eb814de1e4
FatemaElzahraa-Fe2104034 <157298012+FatemaElzahraa-Fe2104034@users.noreply.github.com>
2024-05-10 14:30:12 +0300

    nav css added

--- frh-ecommerce/app/components/top.jsx
+++ frh-ecommerce/app/components/top.jsx
@@ -1,18 +1,13 @@
 
 import React from 'react'
 import styles from '@/app/page.module.css';
+import Link from 'next/link'
 
 export default function top() {
   return (
-    <header>
-    <h1 className={styles.title}>FRH</h1>
-    <ul className={styles.contactLogin}>
-      <li>
-        <a href="#contact" className={`${styles.contactLogina} ${styles.contactLoginfirstchild}`}>Contact Us</a>
-      </li>
-      <a href="" className={styles.contactLogina}>Login</a>
-      <li id="login" />
-    </ul>
-  </header>
+    <div className={styles.topHeader}>
+    <h1 className={styles.topTitle}>FRH</h1>
+    <Link href="" className={styles.topLogout}>Logout</Link>  
+  </div>
   )
 }
